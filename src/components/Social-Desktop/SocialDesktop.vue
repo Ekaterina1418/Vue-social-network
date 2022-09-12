0bff81f5f59da87ac881bb6a7599546f0fe4650f
katerina <eolejnik@icloud.com>
2022-09-12 11:55:51 +0200

    re-push

--- src/components/Social-Desktop/SocialDesktop.vue
+++ src/components/Social-Desktop/SocialDesktop.vue
@@ -1,35 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="block-social">
-    <p class="paragraph-desktop">Or connect with social</p>
-    <button class="facebook-decktop">
-      <div class="text-button">
-        <img src="./image/Facebook2.svg" alt="facebook" />
-        <p class="icons-text">Connect with Facebook</p>
-      </div>
-    </button>
-    <button class="apple-desktop">
-      <div class="text-button">
-        <img src="./image/Apple.svg" alt="apple" />
-        <p class="icons-text">Connect with Aplle</p>
-      </div>
-    </button>
-    <button class="google-desktop">
-      <div class="text-button">
-        <img src="./image/Google2.svg" alt="google" />
-        <p class="icons-text_google">Connect with Google+</p>
-      </div>
-    </button>
-  </div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped>
-@import "./styles/SocialDesktop.scss";
-=======
     <div class="block-social">
         <p class="paragraph-desktop">Or connect with social</p>
         <button class="facebook-decktop" @click="facebook">
@@ -86,5 +55,4 @@ export default class extends Vue {
 
 <style lang="scss" scoped>
 @import './styles/SocialDesktop.scss';
->>>>>>> second_branch
 </style>
